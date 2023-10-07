9c0ef1ad2595e94a3ce7c6a4b6971c019a5a32f9
Datisekai <datly030102@gmail.com>
2023-10-07 22:24:11 +0700

    #4 feat: init project

---
+++ src/test/java/com/example/dsocialserver/DsocialServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsocialserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsocialServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
