5ba5f4bb8ee72e8ef2a1209ce6dba92b153dceb3
duong91002 <nguyenhaiduong9102@gmail.com>
2023-11-21 19:52:13 +0700

    fix message userMessage

--- src/main/java/com/example/dsocialserver/Repositories/MessageRepository.java
+++ src/main/java/com/example/dsocialserver/Repositories/MessageRepository.java
@@ -5,6 +5,7 @@
 package com.example.dsocialserver.Repositories;
 
 import com.example.dsocialserver.Models.Message;
+import com.example.dsocialserver.Models.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
@@ -25,5 +26,6 @@ public interface MessageRepository extends CrudRepository<Message, Object> {
     @Query(value = "SELECT * FROM `message` WHERE message.content LIKE %:q% AND author_id = :authorId AND receive_id= :receiveId ORDER BY `message`.`id` DESC", nativeQuery = true)
     Page<Message> findMessage(Pageable pageable, @Param("authorId") int authorId, @Param("receiveId") int receiveId, @Param("q") String q);
 
- 
+    @Query(value = "SELECT temp.last_message FROM ( SELECT subquery.user_id, MAX(message.content) AS last_message FROM ( SELECT receive_id AS user_id FROM message WHERE author_id = :authorId OR receive_id = :authorId UNION SELECT author_id AS user_id FROM message WHERE author_id = :authorId OR receive_id = :authorId ) AS subquery JOIN message ON (subquery.user_id = message.receive_id OR subquery.user_id = message.author_id) WHERE subquery.user_id <> :authorId GROUP BY subquery.user_id ) AS temp ORDER BY temp.user_id DESC;", nativeQuery = true)
+    Page<Message> findLastMessage(Pageable pageable, @Param("authorId") int authorId);
 }
